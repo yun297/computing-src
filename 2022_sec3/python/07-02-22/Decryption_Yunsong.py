7023c942b00ce322f4ceff9eacd2a934c23c759f
Zhu Yunsong <zhuyunsong02097007@gmail.com>
2022-02-14 10:27:54 +0800

    -

--- 2022_sec3/python/07-02-22/Decryption_Yunsong.py
+++ 2022_sec3/python/07-02-22/Decryption_Yunsong.py
@@ -13,19 +13,19 @@ while condition == True:
         count = 0
         while count < len(cipherText):
             if cipherText[count] == "1": # Checking for 3 digit
-                slicedCipherText = int(cipherText[count] + cipherText[count + 1] + cipherText[count + 2])
+                slicedCipherText = int(cipherText[count] + cipherText[count + 1] + cipherText[count + 2]) # Slice the cipher text into 3 digit
                 if 33 <= slicedCipherText <= 126:
-                    plainText += chr(int(cipherText[count] + cipherText[count + 1] + cipherText[count + 2]))
+                    plainText += chr(int(cipherText[count] + cipherText[count + 1] + cipherText[count + 2])) # Storing the plain text
                     count += 3
-                else:
+                else: # Out of range condition
                     print("The cipher text you keyed in is out of range! Please re-run the code.")
                     break
-            elif cipherText[count] != "1":
-                slicedCipherText = int(cipherText[count] + cipherText[count + 1])
+            elif cipherText[count] != "1": # Checking for 2 digit
+                slicedCipherText = int(cipherText[count] + cipherText[count + 1]) # Slice the cipher text into 2 digit
                 if 33 <= slicedCipherText <= 126:
-                    plainText += chr(int(cipherText[count] + cipherText[count + 1]))
+                    plainText += chr(int(cipherText[count] + cipherText[count + 1])) # Storing the plain text
                     count += 2
-                else:
+                else: # Out of range condition
                     print("The cipher text you keyed in is out of range! Please re-run the code.")
                     break
                 
