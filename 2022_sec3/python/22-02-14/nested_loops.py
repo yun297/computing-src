3ded3c965d432bcddc8d6d6a19b0507b418424e0
Zhu Yunsong <zhuyunsong02097007@gmail.com>
2022-02-15 10:05:15 +0800

    nested loops

--- 2022_sec3/python/22-02-14/nested_loops.py
+++ 2022_sec3/python/22-02-14/nested_loops.py
@@ -1,4 +1,26 @@
+# Times Table
 for i in range(1,11):
     for x in range(1,11):
         print(i*x, end=" ")
+    print("")
+print("\n")
+    
+# 5x5 Star Pattern
+for i in range (5):
+    for i in range (4):
+        print("* " , end = " ")
+    print("* ")
+print("\n")
+
+# 5x5 Star Triangle
+for i in range(5):
+    for x in range(i + 1):
+        print("* ", end=" ")
+    print("")
+print("\n")
+
+# 5x5 Star Upside-down Triangle
+for i in range(5):
+    for x in range(5 - i):
+        print("* ", end=" ")
     print("")
