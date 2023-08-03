70b47543998040f0a7619b73ac173cc0f057efda
Chronos69420 <zhuyunsong02097007@gmail.com>
2023-08-03 10:58:36 +0800

    optional oop assignmetn 2 done

--- 2023_sec4/semester2_python/Assignments/[Optional] Assignment - Recursion 2 & Iteration/main.py
+++ 2023_sec4/semester2_python/Assignments/[Optional] Assignment - Recursion 2 & Iteration/main.py
@@ -10,26 +10,61 @@ def complete(s):
         return True
     
 def foo101(s):
-    s = list(s)
+    if len(s) == 0:
+        return ""
+    elif s[0] == "0":
+        return s[0] + foo101(s[1:])
+    elif s[0] == "1":
+        return foo101(s[1:]) + s[0]
 
-    for i in range(len(s.copy())):
-        if int(s[i-1]) < int(s[i]):
-            s[i], s[i-1] = s[i-1], s[i]
-        
-    print(s)    
-    
-    if complete(s):
-        return s
+# print(foo101("11010010101"))
+# print(foo101("0100"))
+
+# Question 2: foo123
+
+def foo123(s, x = 0, y = 0, z = 0):
+    if len(s) == 0:
+        return "1"*x + "2"*y + "3"*z
+    elif s[0] == "1":
+        return foo123(s[1:], x+1, y, z)
+    elif s[0] == "2":
+        return foo123(s[1:], x, y+1, z)
+    elif s[0] == "3":
+        return foo123(s[1:], x, y, z+1)
+
+# foo123("13233112331231332")
+
+def fooABC(s):
+    check = 0
+    for char in s:
+        if ord(char) < check:
+            break
+        else:
+            check = ord(char)
     else:
-        return foo101(s)
-
-# def foo101(s):
-#     print(s)
+        return s
     
-#     if complete(s):
-#         return s
-#     else:
-#         return foo101(bubbleSort(s))
+    s = list(s)
+    
+    for i in range(1, len(s)):
+        if ord(s[i]) < ord(s[i-1]):
+            s[i], s[i-1] = s[i-1], s[i]
+    
+    return fooABC("".join(s))
 
-print(foo101("11010010101"))
-print(foo101("0100"))
+print(fooABC("imhimhimhihihm"))
+
+def deep_contains(obj, tup):
+    
+    if isinstance(obj, tuple):
+        for value in obj:
+            if value is tup:
+                return True
+            else:
+                return deep_contains(value, tup)
+        else:
+            return False
+    
+# do not touch. needed for public test cases
+x = (1,2)
+a = ((1,2), ((3,4), x), (5,6))
