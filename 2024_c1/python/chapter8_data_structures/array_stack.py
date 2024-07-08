a5d465fd7d9cd9097becb99363733cd72aa677a6
Chronos69420 <zhuyunsong02097007@gmail.com>
2024-07-08 10:11:47 +0800

    array stack

---
+++ 2024_c1/python/chapter8_data_structures/array_stack.py
@@ -0,0 +1,77 @@
+# Dynamic (no size limit)
+
+class ArrayStack:
+    def __init__(self) -> None:
+        self.stack = []
+        
+    def push(self, data):
+        self.stack.append(data)
+        
+    def pop(self):
+        if len(self.stack) == 0:
+            print("Stack is empty.")
+            return
+        
+        return self.stack.pop()
+    
+    def display(self):
+        for item in self.stack[::-1]:
+            print(item)
+        print("End of stack")
+        
+stack1 = ArrayStack
+
+for i in range(5):
+    stack1.push(f"Item {i}")
+    
+stack1.display()
+
+# Fixed-sized
+
+class ArrayStack:
+    CAPACITY = 10
+    def __init__(self):
+        self.data = [''] * ArrayStack.CAPACITY
+        self.top = -1 # same as None
+
+    def push(self,data):
+        if self.top == ArrayStack.CAPACITY -1:
+            print("Stack is full.")
+            return
+        else:
+            self.top += 1
+            self.data[self.top] = data
+            return
+
+    def pop(self):
+        if self.top == -1:
+            return "Stack is empty."
+        else:
+            ret = self.data[self.top]
+            self.top -= 1
+            return ret
+
+    def display(self): # display the stack (qn may want display of array inc. free space)
+        if self.top == -1:
+            print("stack is empty")
+            return
+        for item in self.data[self.top::-1]:
+            print(item)
+        print("end of stack")
+
+staaack = ArrayStack()
+staaack.display()
+print(staaack.pop())
+
+for i in range(7):
+    staaack.push(f"P{i}")
+staaack.display()
+
+print()
+
+print(staaack.pop())
+print()
+
+staaack.display()
+
+print(staaack.data)
