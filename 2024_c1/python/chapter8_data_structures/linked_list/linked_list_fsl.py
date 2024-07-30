9beb0536f2e5c1cf408723f9bbbac7d4113bf3ae
yun297 <zhuyunsong02097007@gmail.com>
2024-07-30 17:05:03 +0800

    Refactor linked list with fsl implementation

---
+++ 2024_c1/python/chapter8_data_structures/linked_list/linked_list_fsl.py
@@ -0,0 +1,62 @@
+class Node:
+    def __init__(self, data = None, next = None) -> None:
+        self.data = data
+        self.next = next
+
+class LinkedList:
+    def __init__(self) -> None:
+        self.root = None
+
+    def push(self, data = Node()):
+        data.next = self.root
+        self.root = data
+
+    def insert(self, data: Node, position):
+        if position == 0:
+            data.next = self.root
+            self.root = data
+        else:
+            count = 0
+            ptr = self.root
+            while ptr.next is not None:
+                if count == position:
+                    data.next = ptr.next
+                    ptr.next = data
+                    return
+                else:
+                    count += 1
+                    ptr = ptr.next
+    
+    def delete(self, target):
+        if self.root is None:
+            print("Linked list is empty!")
+            return
+        else:
+            ptr = self.root
+            while ptr.next is not None:
+                if ptr.next == target:
+                    popped_node = ptr.next
+                    ptr = ptr.next.next
+                    return popped_node
+                else:
+                    ptr = ptr.next
+
+            print("Target not found, no nodes removed!")
+            return
+    
+class Game:
+    def __init__(self, capacity) -> None:
+        self.capacity = capacity
+        self.data = LinkedList()
+        self.free = LinkedList()
+        
+    def create_party(self):
+        for i in range(self.capacity):
+            self.free.push()
+
+    def join_party(self, data: Node, position):
+        self.free.root = data.data
+        self.data.insert(self.free.delete(), position)
+        
+    def leave_party(self, target):
+        self.free.insert(self.data.delete(target), 0)
