231cbe4d988986104b266cf0326896d5ffccf0ff
yun297 <zhuyunsong02097007@gmail.com>
2024-08-05 20:13:36 +0800

    refactor: Refactor binary search tree insert method

---
+++ 2024_c1/python/chapter8_data_structures/bst/binary_search_tree.py
@@ -0,0 +1,41 @@
+class TreeNode:
+    def __init__(self, data, left = None, right = None) -> None:
+        self.data = data
+        self.left = left # left child
+        self.right = right # right child
+    
+    # we will not use setters/getters for data/left/right as they are messy
+
+    # binary search is an efficient algorithm for finding a target value in a sorted list by repeatedly dividing the search interval in half. Starting with the middle element, it compares the target to this element, and if they are not equal, it narrows the search to the left or right half of the list based on whether the target is smaller or larger. This process continues until the target is found or the search range is empty,
+
+    # left child is smaller than right child in a BST
+
+class BSTree:
+    def __init__(self, root = None) -> None:
+        self.root = root
+
+    def insert(self, new_data):
+        new_node = TreeNode(new_data)
+        if self.root == None:
+            self.root == new_node
+            return
+        
+        # travesal
+        ptr = self.root
+
+        while ptr: # as long as pointer has not reached the end of a branch, equivalent to "while True"
+            if new_data > ptr.data: # belongs to right side subtree
+
+                if ptr.right is None: # if right side is empty
+                    ptr.right = new_node # insert as right child
+                    return
+                else:
+                    ptr = ptr.right # shift pointer to the right child
+            
+            else: # belongs to the left side subtree
+
+                if ptr.left is None:
+                    ptr.left = new_node
+                    return
+                else:
+                    ptr = ptr.left # shift pointer to the left child
